3fc89bb70a32e0195945fb787de04df887db1b67
VaniushaR <vaniusha.ra@gmail.com>
2018-10-08 09:58:07 -0500

    Updates

---
+++ precache-manifest.5f85b5ff64a88871f695c80b3bbe4d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nierika/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cf662021d6520eecafe0",
+    "url": "/nierika/static/js/runtime~main.cf662021.js"
+  },
+  {
+    "revision": "f37aab5186999fb09e66",
+    "url": "/nierika/static/js/main.f37aab51.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/nierika/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "f37aab5186999fb09e66",
+    "url": "/nierika/static/css/main.16c17991.chunk.css"
+  },
+  {
+    "revision": "5f8525f3f2d9428cd43c884c47e64926",
+    "url": "/nierika/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/nierika/precache-manifest.f8aa2cfa146d0b283ed6ab15d5aeef22.js"
+  "/nierika/precache-manifest.5f85b5ff64a88871f695c80b3bbe4d81.js"
 );
 
 workbox.clientsClaim();
