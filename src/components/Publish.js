ae6797ed5c0d0ab955598eea52ba7a199fbed1cb
VaniushaR <vaniusha.ra@gmail.com>
2019-07-11 01:14:25 -0500

    Form Semantic UI

--- src/components/Publish.js
+++ src/components/Publish.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import { db } from './Credentials';
 import { currenUser, picCurrenUser } from './Login';
-import { Feed, Icon, Button, Divider, Segment } from 'semantic-ui-react';
+import LanguagesOptions from './Languages';
+import LocationOptions from './Locations';
+import {
+  Feed,
+  Icon,
+  Button,
+  Divider,
+  Segment,
+  Checkbox,
+  Grid,
+  Label,
+  Select,
+  Input,
+  Header
+} from 'semantic-ui-react';
 
 const posting = textPosted => {
   const postDate = new Date();
+
+  const locationOptions = [
+    { name: 'Baja California Sur, San Pancho' },
+    { name: 'Puebla, San Andrés' },
+    { name: 'Veracruz, Tlacotalpan' }
+  ];
 
   db.collection('timeLine')
     .add({
@@ -49,50 +69,131 @@ class Publish extends Component {
     return (
       <div className="main">
         <section className="timeLine">
-          <div className="card text-center">
-            <div className="card-header">
-              <img src={picCurrenUser} alt={currenUser} className="profile" />
-              <h3> {currenUser}</h3>
-            </div>
-            <div className="card-body">
-              <Segment>
-                <h5 className="card-title">
-                  Comparte con la comunidad una frase o texto en una lengua
-                  originaria
-                </h5>
-                <input
-                  value={this.state.value}
-                  onChange={this.handleChange.bind(this)}
-                />
-                <Divider section />
-                <Button onClick={this.handleClick.bind(this)} color="green">
-                  Publicar{' '}
-                </Button>
-                <p className="card-text" />
-                <a className="btn btn-Success">
-                  Añadir información adicional para colaborar a la investigación
-                  de la lengua originaria
-                </a>
-              </Segment>
-            </div>
-
-            <Feed>
-              <Feed.Event>
-                <Feed.Content>
-                  <Feed.Meta>
-                    <Feed.Like>
-                      <Icon name="like" />4 Likes
-                    </Feed.Like>
-                    <input
-                      placeholder="¿A qué lengua pertenece?"
-                      id="Language"
+          <Segment raised>
+            <Grid columns="equal">
+              <Grid.Row>
+                <Grid.Column>
+                  <Header as="h2">
+                    <img
+                      src={picCurrenUser}
+                      alt={currenUser}
+                      className="profile"
                     />
-                  </Feed.Meta>
-                </Feed.Content>
-              </Feed.Event>
-            </Feed>
-            <div className="card-footer text-muted"> </div>
-          </div>
+                    {currenUser}
+                  </Header>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Segment>
+                    <h5>
+                      Comparte con la comunidad una frase o texto en una lengua
+                      originaria
+                    </h5>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Segment>
+                    <Label attached="top">
+                      Selecciona la Lengua Originaria
+                    </Label>
+
+                    <LanguagesOptions />
+                  </Segment>
+                </Grid.Column>
+                <Grid.Column>
+                  <Segment>
+                    <Label attached="top">Selecciona su localidad</Label>
+                    <LocationOptions />
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+
+              <Grid.Row>
+                <Grid.Column>
+                  <Segment padded>
+                    <Label attached="top">Escribe en Lengua Originaria </Label>
+                    <Input
+                      className="input-form"
+                      value={this.state.value}
+                      onChange={this.handleChange.bind(this)}
+                      placeholder="A bird in a branch have not fear because it is confidence is on its wings"
+                    />
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Segment padded>
+                    <Label attached="top">
+                      Agrega la traducción al español
+                    </Label>
+                    <Input
+                      className="input-form"
+                      placeholder="Los usuarios validarán la traducción de tu texto..."
+                    />
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Segment padded>
+                    <Label attached="top right">Agrega una imagen</Label>
+                    <Button color="teal">Buscar en mis archivos</Button>
+                    <Button color="orange">Acceder a la cámara</Button>
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+              <Grid.Row>
+                <Grid.Column>
+                  <Segment>
+                    <Label attached="top right">
+                      Selecciona el tipo de contenido
+                    </Label>
+                    <Checkbox label="Poesía" />
+                    <Checkbox label="Narración verídica" />
+                    <Checkbox label="Noticia" />
+                    <Checkbox label="Frase o Refrán" />
+                    <Checkbox label="Cuento" />
+                    <Checkbox label="Mito o Leyenda" />
+                    <Checkbox label="Gramática" />
+                    <Checkbox label="Vocabulario" />
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+
+            <Divider section />
+
+            <Segment compact>
+              <Checkbox toggle />
+              <label>
+                Permitir que mi publicación y mis datos de hablante sean
+                utilizados para investigación y fomento al conocimiento de las
+                Lenguas Originarias
+              </label>
+
+              <Button onClick={this.handleClick.bind(this)} color="green">
+                Publicar{' '}
+              </Button>
+            </Segment>
+          </Segment>
+
+          <Feed>
+            <Feed.Event>
+              <Feed.Content>
+                <Feed.Meta>
+                  <Feed.Like>
+                    <Icon name="like" />4 Likes
+                  </Feed.Like>
+                  <input placeholder="¿A qué lengua pertenece?" id="Language" />
+                </Feed.Meta>
+              </Feed.Content>
+            </Feed.Event>
+          </Feed>
+          <div className="card-footer text-muted"> </div>
         </section>
       </div>
     );
